d68e8f8831519443d5d310e8de0683befde5eb28
bai-y <cold880512@gmail.com>
2018-11-16 11:12:53 +0800

    new init

---
+++ Assets/Script/SetDominoes.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SetDominoes : MonoBehaviour
+{
+    public GameObject dominoePrefab;
+    public GameObject dominoeEnd;
+    public GameObject dominoestart;
+    public int NumCenterDom = 25;
+    void Start()
+    {
+        Vector3 dir = dominoeEnd.transform.position - dominoestart.transform.position;
+        float interval = dir.magnitude / (NumCenterDom + 1);
+        dir.Normalize();
+        for (int i = 1; i <= NumCenterDom; i++)
+        {
+            GameObject newDom = (GameObject)Instantiate(dominoePrefab);
+            newDom.transform.position = dominoestart.transform.position + dir * interval * i;
+            newDom.transform.rotation = dominoestart.transform.rotation;
+        }
+    }
+    public float force = 4.0f;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            dominoestart.GetComponent<Rigidbody>().AddForce(force, 0, 0, ForceMode.Impulse);
+        }
+    }
+}
